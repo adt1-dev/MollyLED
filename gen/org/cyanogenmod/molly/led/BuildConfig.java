c977e828049168689c737b199401eda710be8cf3
Greg Willard <wickett06@gmail.com>
2015-12-13 22:36:16 -0500

    upload all my stuff so far

---
+++ gen/org/cyanogenmod/molly/led/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.cyanogenmod.molly.led;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/cyanogenmod/molly/led/MutableString.java
@@ -0,0 +1,14 @@
+package org.cyanogenmod.molly.led;
+
+public class MutableString {
+    private String value;
+    public MutableString(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
