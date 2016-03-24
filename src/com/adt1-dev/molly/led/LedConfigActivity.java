52253803f73fff0c3824e4cf77418264398a492d
npjohnson <johnsonnolen@gmail.com>
2016-03-23 22:04:40 -0400

    MollyLED: org.cyanogenmod ==> com.adt1-dev

--- src/com/adt1-dev/molly/led/LedConfigActivity.java
+++ src/com/adt1-dev/molly/led/LedConfigActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2015 The ADT-1 Development Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.cyanogenmod.molly.led;
+package com.adt1-dev.molly.led;
 
 import android.app.*;
 import android.os.*;
